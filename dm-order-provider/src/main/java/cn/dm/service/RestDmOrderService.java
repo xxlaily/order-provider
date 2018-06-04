af66a27047c67de1e1ca91f7595bd501a4494263
微尘 <1044732267@qq.com>
2018-06-04 18:39:27 +0800

    Upload dm-order-provider/src/main/resources/application.yml dm-order-provider/pom.xml dm-order-provider/src/main/java/cn/dm/service/RestDmOrderService.java dm-order-provider/src/main/java/cn/dm/OrderProviderLauncher.java dm-order-provider/src/main/resources/logback-spring.xml dm-order-provider/src/main/java/cn/dm/service/RestDmOrderLinkUserService.java

---
+++ dm-order-provider/src/main/java/cn/dm/service/RestDmOrderService.java
@@ -0,0 +1,56 @@
+package cn.dm.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import cn.dm.mapper.DmOrderMapper;
+import cn.dm.pojo.DmOrder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by zezhong.shang on 18-5-15.
+ */
+@RestController
+public class RestDmOrderService {
+
+    @Autowired
+    private DmOrderMapper dmOrderMapper;
+
+    @RequestMapping(value = "/getDmOrderById", method = RequestMethod.POST)
+    public DmOrder getDmOrderById(@RequestParam("id") Long id) throws Exception {
+        return dmOrderMapper.getDmOrderById(id);
+    }
+
+    @RequestMapping(value = "/getDmOrderListByMap", method = RequestMethod.POST)
+    public List<DmOrder> getDmOrderListByMap(@RequestBody Map<String, Object> param) throws Exception {
+        return dmOrderMapper.getDmOrderListByMap(param);
+    }
+
+    @RequestMapping(value = "/getDmOrderCountByMap", method = RequestMethod.POST)
+    public Integer getDmOrderCountByMap(@RequestBody Map<String, Object> param) throws Exception {
+        return dmOrderMapper.getDmOrderCountByMap(param);
+    }
+
+    @RequestMapping(value = "/qdtxAddDmOrder", method = RequestMethod.POST)
+    public Integer qdtxAddDmOrder(@RequestBody DmOrder dmOrder) throws Exception {
+        dmOrder.setCreatedTime(new Date());
+        return dmOrderMapper.insertDmOrder(dmOrder);
+    }
+
+    @RequestMapping(value = "/qdtxModifyDmOrder", method = RequestMethod.POST)
+    public Integer qdtxModifyDmOrder(@RequestBody DmOrder dmOrder) throws Exception {
+        dmOrder.setUpdatedTime(new Date());
+        return dmOrderMapper.updateDmOrder(dmOrder);
+    }
+
+    @RequestMapping(value = "/getDmOrderListByOrderNoOrDate", method = RequestMethod.POST)
+    public List<DmOrder> getDmOrderListByOrderNoOrDate(@RequestBody Map<String, Object> param) throws Exception {
+        return dmOrderMapper.getDmOrderListByOrderNoOrDate(param);
+    }
+}
