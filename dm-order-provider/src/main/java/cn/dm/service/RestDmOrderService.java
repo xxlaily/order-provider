e92425fe3e85c375f8b8b0ddd29a1aaea30c56cf
hai.dong <sdodh366@163.com>
2018-06-27 11:09:34 +0800

    判断订单状态

--- dm-order-provider/src/main/java/cn/dm/service/RestDmOrderService.java
+++ dm-order-provider/src/main/java/cn/dm/service/RestDmOrderService.java
@@ -1,5 +1,6 @@
 package cn.dm.service;
 
+import cn.dm.common.EmptyUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -75,5 +76,15 @@ public class RestDmOrderService {
     @RequestMapping(value = "/getDmOrderByOrderTypeAndTime", method = RequestMethod.POST)
     public List<DmOrder> getDmOrderByOrderTypeAndTime() throws Exception{
         return dmOrderMapper.getDmOrderByOrderTypeAndTime();
+    }
+
+    @RequestMapping(value = "/processed", method = RequestMethod.POST)
+    public boolean processed(@RequestParam("orderNo")String orderNo, Integer flag) throws Exception {
+        if(EmptyUtils.isEmpty(orderNo)||EmptyUtils.isEmpty(flag)){
+            return false;
+        }
+        DmOrder dmOrder = this.getDmOrderByOrderNo(orderNo);
+        String tradeNo = flag == 0?dmOrder.getWxTradeNo():dmOrder.getAliTradeNo();
+        return dmOrder.getOrderType().equals(2) && !EmptyUtils.isEmpty(tradeNo);
     }
 }
